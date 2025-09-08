2d80a58f218520c16ab3ba29afa6a7a1ed33fbab
urko27 <manu.urko@outlook.com>
2025-09-08 19:15:18 -0300

    first commit

---
+++ src/Util.hs
@@ -0,0 +1,21 @@
+module Util where
+
+-- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
+-- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+alinearDerecha :: Int -> String -> String
+alinearDerecha n s = error "COMPLETAR EJERCICIO 1"
+
+-- | Dado un índice y una función, actualiza el elemento en la posición del índice
+-- aplicando la función al valor actual. Si el índice está fuera de los límites
+-- de la lista, devuelve la lista sin cambios.
+-- El primer elemento de la lista es el índice 0.
+actualizarElem :: Int -> (a -> a) -> [a] -> [a]
+actualizarElem n f xs = error "COMPLETAR EJERCICIO 2"
+
+-- | infinito positivo (Haskell no tiene literal para +infinito)
+infinitoPositivo :: Float
+infinitoPositivo = 1 / 0
+
+-- | infinito negativo (Haskell no tiene literal para -infinito)
+infinitoNegativo :: Float
+infinitoNegativo = -(1 / 0)
