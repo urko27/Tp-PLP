00e6aedd1485471f6095ff1f4a124dab278d56bc
urko27 <manu.urko@outlook.com>
2025-10-12 12:12:26 -0300

    tests foldExpr

--- test/Main.hs
+++ test/Main.hs
@@ -184,24 +184,23 @@ testsFold :: Test
 testsFold =
   test
     [
-      1 ~?= 1
-      -- fst (foldExpr                   -- Sumamos 1 a los valores numéricos de la expresión
-      --   (\c g -> (Const (c+1), g))
-      --   (\a b g -> (Rango (a+1) (b+1), g))
-      --   (\(resP, genP) (resQ, genQ) -> (Suma resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Resta resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Mult resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Div resP resQ, genQ))
-      --   (Suma (Const 2.0) (Rango 1 5)) genFijo) ~?= Suma (Const 3.0) (Rango 2 6),
-      --   fst (foldExpr                 -- "Invertimos" las operaciones, suma <-> resta y mult <-> div
-      --   (\c g -> (Const c, g))
-      --   (\a b g -> (Rango a b, g))
-      --   (\(resP, genP) (resQ, genQ) -> (Resta resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Suma resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Div resP resQ, genQ))
-      --   (\(resP, genP) (resQ, genQ) -> (Mult resP resQ, genQ))
-      --   (Div (Resta (Suma (Const 2.0) (Rango 1 5)) (Mult (Const 1.0) (Rango 4 8))) (Const 2.0)) genFijo) ~?=
-      --     (Mult (Suma (Resta (Const 2.0) (Rango 1 5)) (Div (Const 1.0) (Rango 4 8))) (Const 2.0))
+      foldExpr                   -- Sumamos 1 a los valores numéricos de la expresión
+        (\c -> Const (c+1))
+        (\a b -> Rango (a+1) (b+1))
+        (\p q -> Suma p q)
+        (\p q -> Resta p q)
+        (\p q -> Mult p q)
+        (\p q -> Div p q)
+        (Suma (Const 2.0) (Rango 1 5)) ~?= Suma (Const 3.0) (Rango 2 6),
+      foldExpr                 -- "Invertimos" las operaciones, suma <-> resta y mult <-> div
+        (\c -> Const c)
+        (\a b -> Rango a b)
+        (\p q -> Resta p q)
+        (\p q -> Suma p q)
+        (\p q -> Div p q)
+        (\p q -> Mult p q)
+        (Div (Resta (Suma (Const 2.0) (Rango 1 5)) (Mult (Const 1.0) (Rango 4 8))) (Const 2.0)) ~?=
+        (Mult (Suma (Resta (Const 2.0) (Rango 1 5)) (Div (Const 1.0) (Rango 4 8))) (Const 2.0))
     ]
 
 testsEval :: Test
